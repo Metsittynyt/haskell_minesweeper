dc75bb2e903324969633bd3258f4c0dbf26a77f7
elvleh <elvleh@utu.fi>
2024-04-08 12:33:37 +0300

    added way to exit mid game

--- app/Main.hs
+++ app/Main.hs
@@ -23,14 +23,19 @@ mainLoop gameState = do
     Ongoing -> do
       putStrLn "Enter row and column to reveal, separated by space:"
       input <- getLine
-      let parsedInput = map readMaybe . words $ input :: [Maybe Int]
-      case parsedInput of
-        [Just row, Just col] -> do
-          let newState = revealCell (col, row) gameState
-          printBoard (board newState) -- Print the updated board.
-          case gameStatus newState of
-            Lost -> putStrLn "Boom! You've hit a mine. Game over."
-            _    -> mainLoop newState
-        _ -> putStrLn "Invalid input. Please enter valid row and column numbers separated by a space."
+      if input == "quit" then do
+        putStrLn "Game over. Thanks for playing!"
+        return ()
+      else do
+        let parsedInput = map readMaybe . words $ input :: [Maybe Int]
+        case parsedInput of
+          [Just row, Just col] -> do
+            let newState = revealCell (col, row) gameState
+            printBoard (board newState) -- Print the updated board.
+            case gameStatus newState of
+              Lost -> putStrLn "Boom! You've hit a mine. Game over."
+              _    -> mainLoop newState
+          _ -> putStrLn "Invalid input. Please enter valid row and column numbers separated by a space."
     Won -> putStrLn "Congratulations! You've won the game!"
     Lost -> putStrLn "Unexpected game over." -- This case might now be redundant.
+    
