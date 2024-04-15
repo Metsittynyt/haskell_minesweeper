18375c2cd2a47241a7d087ecce2f3e5aec47ed62
elvleh <elvleh@utu.fi>
2024-04-15 13:06:28 +0300

    added numbers into adjasent cells and fixed flag functionality

--- app/GameState.hs
+++ app/GameState.hs
@@ -10,6 +10,7 @@ module GameState
 where
 
 import Board
+import Graphics.Gloss
 import Graphics.Gloss.Interface.IO.Game
 import Input
 
@@ -23,18 +24,30 @@ data GameStatus = Ongoing | Won | Lost deriving (Show, Eq)
 drawGame :: GameState -> IO Picture
 drawGame gameState =
   return $
-    translate (-144) (-144) $
-      scale 32 32 $
+    translate (-144) (-144) $ -- Adjust the translation to center the board
+      scale 32 32 $ -- Scale each cell to 32x32 pixels
         pictures
           [ case safeGetCell (board gameState) (x, 9 - y) of
               Just cell ->
                 translate (fromIntegral x) (fromIntegral y) $
-                  color (chooseColor cell) $
-                    rectangleSolid 0.9 0.9
+                  pictures
+                    [ color (chooseColor cell) $ rectangleSolid 0.9 0.9, -- Draw the cell background
+                      drawCellText cell -- Call function to draw the number of adjacent mines
+                    ]
               Nothing -> blank -- if the cell is out of bounds, draw nothing
             | x <- [0 .. 9],
               y <- [0 .. 9]
           ]
+
+-- Function to draw the number of adjacent mines on the cell
+drawCellText :: Cell -> Picture
+drawCellText cell
+  | isRevealed cell && not (isMine cell) && adjacentMines cell > 0 =
+      translate (-0.2) (-0.2) $ -- Adjust translation to center the text in the cell
+        scale 0.005 0.005 $ -- Adjust scaling to fit the text size
+          color black $ -- Set text color to black
+            text (show $ adjacentMines cell) -- Convert the number of adjacent mines to text
+  | otherwise = blank
 
 chooseColor :: Cell -> Color
 chooseColor cell
@@ -50,23 +63,28 @@ chooseColor cell
 
 handleEvent :: Event -> GameState -> IO GameState
 handleEvent event gameState = case event of
+  -- Handle left mouse button click (reveal cell).
   EventKey (MouseButton LeftButton) Down _ mousePos -> do
-    -- Translate mouse position and scale it according to the cell size
-    let (mouseX, mouseY) = invertMouseCoordinates mousePos
-
-    -- Calculate the column and row in the grid from the mouse position
-    let (col, row) = (floor mouseX, floor mouseY)
-
-    -- Debug information
-    putStrLn $ "Clicked pixel coordinates: " ++ show mousePos
-    putStrLn $ "Converted to grid coordinates (col, row): " ++ show (col, row)
-
-    -- Check if the click is within the bounds of the board and reveal the cell if it is
+    let (col, row) = convertMouseCoords mousePos
     let validMove = isValidMove (board gameState) (col, row)
     if validMove
       then return $ revealCell (col, row) gameState
       else return gameState -- Ignore if the move is invalid
-  _ -> return gameState -- Ignore other events
+
+  -- Handle right mouse button click (toggle flag).
+  EventKey (MouseButton RightButton) Down _ mousePos -> do
+    let (col, row) = convertMouseCoords mousePos
+    let validMove = isValidMove (board gameState) (col, row)
+    if validMove
+      then return $ toggleFlag (col, row) gameState
+      else return gameState -- Ignore if the move is invalid
+  _ -> return gameState -- Handle other events
+
+-- Helper function to convert screen coordinates to grid coordinates
+convertMouseCoords :: Point -> (Int, Int)
+convertMouseCoords (x, y) =
+  let (gridX, gridY) = invertMouseCoordinates (x, y)
+   in (floor gridX, floor gridY)
 
 -- Function to convert the screen coordinates of the mouse to grid coordinates
 invertMouseCoordinates :: Point -> (Float, Float)
