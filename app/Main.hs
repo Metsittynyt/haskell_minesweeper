deb9a666783b992e9c075d3a3a2d8dab6319f234
elvleh <elvleh@utu.fi>
2024-04-23 13:43:23 +0300

    remove unnecessary stuff

--- app/Main.hs
+++ app/Main.hs
@@ -1,29 +1,25 @@
 module Main where
 
-import System.Random
-import Data.List (intersperse)
-import Text.Read (readMaybe)
+import System.Random ()
+import Data.List ()
+import Text.Read ()
 import Graphics.Gloss.Interface.IO.Game
 
-import Board
+import Board ()
 import GameState
-import Input
+    ( drawGame, handleEvent, initialGameState, updateGame )
+import Input ()
+
 
 -- Main function
 main :: IO ()
 main = do
-    -- Welcome message and quick guide on how to play.
-    putStrLn "Welcome to Minesweeper!"
-    putStrLn ""
-    putStrLn "The board size is 10 x 10. There are total of 10 bombs in hiding."
-    putStrLn "You reveal cells by giving column and row number, separated by space."
-    putStrLn "Column an row numbers go from 0 to 9."
-
     -- Initiate the game state.
     gameState <- initialGameState
 
     -- Setup the display window
     let windowSize = 10 * 32 + 2 * 40
     let window = InWindow "Minesweeper" (windowSize, windowSize + 100) (100, 100)
+
     -- Run the game using playIO from Gloss
     playIO window white 30 gameState drawGame handleEvent updateGame
