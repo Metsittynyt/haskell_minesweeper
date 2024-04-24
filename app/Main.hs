4c5ea0bf6246d1bb9fa2877752b3777d88a643e3
elvleh <elvleh@utu.fi>
2024-04-24 14:33:17 +0300

    added buttons

--- app/Main.hs
+++ app/Main.hs
@@ -4,12 +4,15 @@ import System.Random ()
 import Data.List ()
 import Text.Read ()
 import Graphics.Gloss.Interface.IO.Game
+import System.Exit (exitSuccess)
+
 
 import Board ()
 import GameState
-    ( initialGameState, handleEvent, updateGame )
-import Draw (drawGame)
+    ( GameState, initialGameState, updateGame, handleEvent )  -- Import GameState type here
 import Input ()
+import Draw (drawGame)
+import Button (drawButtons, handleButtonEvent)
 
 
 -- Main function
@@ -23,4 +26,17 @@ main = do
     let window = InWindow "Minesweeper" (windowSize, windowSize + 100) (100, 100)
 
     -- Run the game using playIO from Gloss
-    playIO window white 30 gameState drawGame handleEvent updateGame
+    playIO window white 30 gameState drawAll handleAllEvents updateGame
+
+-- Drawing function that combines game and button graphics
+drawAll :: GameState -> IO Picture
+drawAll gameState = do
+  gamePic <- drawGame gameState  -- Draw the game state
+  let buttonPics = drawButtons gameState  -- Draw buttons from Button.hs
+  return $ pictures [gamePic, buttonPics]
+
+-- Handling all game events, including button interactions
+handleAllEvents :: Event -> GameState -> IO GameState
+handleAllEvents event gameState = do
+  gameState' <- handleButtonEvent event gameState  -- First handle button events
+  handleEvent event gameState'  -- Then handle general game events
